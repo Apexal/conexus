f0fd2b1c36a5c1ec5bed26ea30965cdff6e6b5ca
Frank Matranga <thefrankmatranga@gmail.com>
2017-04-18 16:46:18 -0400

    Persistent associations with YAML

--- bot.rb
+++ bot.rb
@@ -3,6 +3,7 @@ if ARGV.length != 2
   exit
 end
 
+require 'yaml'
 require 'discordrb'
 
 # This hash will store voice channel_ids mapped to text_channel ids
@@ -10,7 +11,9 @@ require 'discordrb'
 #   "267526886454722560": "295714345344565249",
 #   etc.
 # }
-ASSOCIATIONS = Hash.new
+ASSOCIATIONS = YAML.load_file('associations.yaml')
+ASSOCIATIONS ||= Hash.new
+
 OLD_VOICE_STATES = Hash.new
 
 # These are the perms given to people for a associated voice-channel
@@ -27,7 +30,14 @@ bot.server_create { |event| setup_server(event.server) }
 
 def setup_server(server)
   puts "Setting up [#{server.name}]"
-  server.text_channels.select { |vc| vc.name == 'voice-channel' }.map(&:delete) # Remove previous text voice-channel's that are abandoned
+  trim_associations(server)
+  server.text_channels.select { |tc| tc.name == 'voice-channel' }.each do |tc|
+    return tc.delete unless ASSOCIATIONS.values.include?(tc.id)
+    vc = server.voice_channels.find { |vc| vc.id == ASSOCIATIONS.key(tc) }
+    tc.users.select { |u| !vc.users.include?(u) }.each do |u|
+      tc.define_overwrite(u, 0, 0)
+    end
+  end
   server.voice_channels.each { |vc| associate(vc) }
   OLD_VOICE_STATES[server.id] = server.voice_states.clone
 end
@@ -37,6 +47,13 @@ def simplify_voice_states(voice_states)
   clone.each { |user_id, state| clone[user_id] = state.voice_channel }
   
   return clone
+end
+
+def trim_associations(server)
+  ASSOCIATIONS.each do |vc_id, tc_id|
+    ASSOCIATIONS.delete(vc_id) if tc_id.nil? || server.voice_channels.find { |vc| vc.id == vc_id }.nil?
+  end
+  save
 end
 
 def associate(voice_channel)
@@ -56,6 +73,7 @@ def associate(voice_channel)
 
     text_channel.define_overwrite(voice_channel.server.roles.find { |r| r.id == voice_channel.server.id }, 0, TEXT_PERMS) # Set default perms as invisible
     ASSOCIATIONS[voice_channel.id] = text_channel.id # Associate the two 
+    save
   end
 
   text_channel
@@ -85,6 +103,7 @@ end
 # VOICE-CHANNEL DELETED
 bot.channel_delete(type: 2) do |event|
   event.server.text_channels.select { |tc| tc.id == ASSOCIATIONS[event.id] }.map(&:delete)
+  trim_associations(event.server)
 end
 
 bot.voice_state_update do |event|
@@ -92,13 +111,18 @@ bot.voice_state_update do |event|
   current = simplify_voice_states(event.server.voice_states)
   member = event.user.on(event.server)
 
-  if event.server.voice_states != old || current[member.id].voice_channel != old[member.id].voice_channel
+  if event.old_channel != event.channel #current[member.id] != old[member.id]
     # Something has happened
     handle_user_change(:leave, event.old_channel, member) unless event.old_channel.nil?
     handle_user_change(:join, event.channel, member) unless event.channel.nil?
-  end
 
-  OLD_VOICE_STATES[event.server.id] = event.server.voice_states.clone
+    OLD_VOICE_STATES[event.server.id] = event.server.voice_states.clone
+  end
 end
 
+def save
+  File.open('associations.yaml', 'w') {|f| f.write ASSOCIATIONS.to_yaml }
+end
+
+puts "Oauth url: #{bot.invite_url}+&permissions=8"
 bot.run
